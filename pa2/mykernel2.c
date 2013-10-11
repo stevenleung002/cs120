b7d3d7037a24e784836eb60f2b746d73207d50b7
dotcom900825 <dotcom900825@gmail.com>
2013-10-11 00:12:45 -0700

    round robin

--- pa2/mykernel2.c
+++ pa2/mykernel2.c
@@ -11,7 +11,7 @@
 #include "sys.h"
 #include "mykernel2.h"
 
-#define TIMERINTERVAL 1  /* in ticks (tick = 10 msec) */
+#define TIMERINTERVAL 10  /* in ticks (tick = 10 msec) */
 
 #define QUEUESIZE 1000
 /*  A sample process table.  You may change this any way you wish.
@@ -294,7 +294,6 @@ int SchedProc ()
   case ROUNDROBIN:
     if ( !empty(&pid_queue) ){
       ror_pid = get_queue_next(&pid_queue);
-      Printf("Scheduling Proc %d\n", ror_pid);
       return ror_pid;
     }
     break;
