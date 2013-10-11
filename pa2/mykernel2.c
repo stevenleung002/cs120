f92c5bdcdd6d3f14693439601de9e957800d9b3b
dotcom900825 <dotcom900825@gmail.com>
2013-10-10 20:08:05 -0700

    test fifo

--- pa2/mykernel2.c
+++ pa2/mykernel2.c
@@ -251,7 +251,7 @@ int SchedProc ()
   case FIFO:
     if ( !empty(&pid_queue) ){
       fifo_pid = dequeue(&pid_queue);
-      Printf("Scheduling Proc %d\n", lifo_pid);
+      Printf("Scheduling Proc %d\n", fifo_pid);
       return fifo_pid;
     }
 
