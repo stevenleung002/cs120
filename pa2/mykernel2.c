9771443eab47d2586d2d7abad6054a267017518e
dotcom900825 <dotcom900825@gmail.com>
2013-10-17 10:51:09 -0700

    mykernel2.c

--- pa2/mykernel2.c
+++ pa2/mykernel2.c
@@ -69,7 +69,7 @@ int get_unfair_pid()
     if(proctab[i].valid == 1 ){
       Printf("requested %f \n", proctab[i].requested);
       Printf("utilization %f \n", proctab[i].utilization);
-      Printf("proc %d \n", proctab[i].pid);
+      Printf("proc %d ran_slot %d, alive_slot %d  \n", proctab[i].pid, proctab[i].ran_slot, proctab[i].alive_slot);
     }
 
     if (proctab[i].valid == 0){
