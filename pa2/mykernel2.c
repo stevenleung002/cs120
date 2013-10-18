fe76589b50ec7ff43162399d48a9cc8f857e0004
dotcom900825 <dotcom900825@gmail.com>
2013-10-18 15:15:48 -0700

    mykernel2.c

--- pa2/mykernel2.c
+++ pa2/mykernel2.c
@@ -269,7 +269,7 @@ void InitSched ()
    * called, thus leaving the policy to whatever we chose to test.
    */
   if (GetSchedPolicy () == NOSCHEDPOLICY) { /* leave as is */
-    SetSchedPolicy (ROUNDROBIN);   /* set policy here */
+    SetSchedPolicy (PROPORTIONAL);   /* set policy here */
   }
 
   /* Initialize all your data structures here */
