4eca5ee9b2eea7ca76e38d197918559dc108c493
dotcom900825 <dotcom900825@gmail.com>
2013-10-18 16:45:39 -0700

    mykernel2.c

--- pa2/pa2c.c
+++ pa2/pa2c.c
@@ -133,12 +133,13 @@ void Main ()
 		if (Fork () == 0) {
 
 			/* Process 4 */
+			RequestCPUrate (1, 10);
 			SlowPrintf (6, "444444444444444444");
 			Exit ();
 		}
 
 		/* Process 2 */
-		//RequestCPUrate (3, 10);
+		RequestCPUrate (3, 10);
 		SlowPrintf (6, "222222222222222222");
 		Exit ();
 	}
@@ -146,13 +147,13 @@ void Main ()
 	if (Fork () == 0) {
 
 		/* Process 3 */
-		//RequestCPUrate (2, 10);
+		RequestCPUrate (2, 10);
 		SlowPrintf (6, "333333333333333333");
 		Exit ();
 	}
 
 	/* Process 1 */
-	//RequestCPUrate (4, 10);
+	RequestCPUrate (4, 10);
 	SlowPrintf (6, "111111111111111111");
 	Exit ();
 }
