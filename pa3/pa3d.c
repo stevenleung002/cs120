c7cd0b98b2597145572cbbd1099e53ab942c13e5
dotcom900825 <dotcom900825@gmail.com>
2013-11-02 23:38:45 -0700

    retry test case 1

--- pa3/pa3d.c
+++ pa3/pa3d.c
@@ -171,7 +171,43 @@ void Main ()
 
   if (Fork () == 0) {
     Delay (0);
-    driveRoad (EAST, 10);
+    driveRoad (WEST, 10);
+    Exit ();
+  }
+
+  if (Fork () == 0) {
+    Delay (0);
+    driveRoad (WEST, 20);
+    Exit ();
+  }
+
+  if (Fork () == 0) {
+    Delay (0);
+    driveRoad (WEST, 30);
+    Exit ();
+  }
+
+  if (Fork () == 0) {
+    Delay (0);
+    driveRoad (WEST, 40);
+    Exit ();
+  }
+
+  if (Fork () == 0) {
+    Delay (0);
+    driveRoad (WEST, 50);
+    Exit ();
+  }
+
+  if (Fork () == 0) {
+    Delay (0);
+    driveRoad (WEST, 60);
+    Exit ();
+  }
+
+  if (Fork () == 0) {
+    Delay (0);
+    driveRoad (WEST, 70);
     Exit ();
   }
 
@@ -181,10 +217,18 @@ void Main ()
     Exit ();
   }
 
+  if (Fork () == 0) {
+    Delay (0);
+    driveRoad (WEST, 90);
+    Exit ();
+  }
+
   driveRoad (WEST, 5);
 
   Exit ();
 }
+
+
 
 
 
