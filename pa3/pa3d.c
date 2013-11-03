bd6ed1fe2031cf46c36c3832886f8a9e5258c8ac
dotcom900825 <dotcom900825@gmail.com>
2013-11-02 21:50:34 -0700

    test case 1

--- pa3/pa3d.c
+++ pa3/pa3d.c
@@ -171,13 +171,55 @@ void Main ()
 
   if (Fork () == 0) {
     Delay (0);
-    driveRoad (EAST, 10);
+    driveRoad (WEST, 10);
+    Exit ();
+  }
+
+  if (Fork () == 0) {
+    Delay (0);
+    driveRoad (WEST, 20);
+    Exit ();
+  }
+
+  if (Fork () == 0) {
+    Delay (0);
+    driveRoad (WEST, 30);
+    Exit ();
+  }
+
+  if (Fork () == 0) {
+    Delay (0);
+    driveRoad (WEST, 40);
+    Exit ();
+  }
+
+  if (Fork () == 0) {
+    Delay (0);
+    driveRoad (WEST, 50);
+    Exit ();
+  }
+
+  if (Fork () == 0) {
+    Delay (0);
+    driveRoad (WEST, 60);
+    Exit ();
+  }
+
+  if (Fork () == 0) {
+    Delay (0);
+    driveRoad (WEST, 70);
     Exit ();
   }
 
   if (Fork () == 0) {
     Delay (0);
     driveRoad (WEST, 80);
+    Exit ();
+  }
+
+  if (Fork () == 0) {
+    Delay (0);
+    driveRoad (WEST, 90);
     Exit ();
   }
 
