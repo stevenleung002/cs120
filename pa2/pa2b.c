991f588a0462eb99cd187097d78331962845cabf
dotcom900825 <dotcom900825@gmail.com>
2013-11-04 09:27:49 -0800

    test pa2 fifo

--- pa2/pa2b.c
+++ pa2/pa2b.c
@@ -97,27 +97,29 @@
 #include <stdio.h>
 #include "aux.h"
 #include "umix.h"
-
 void Main ()
 {
-	if (Fork () == 0) {
-
-		if (Fork () == 0) {
-
-			SlowPrintf (7, "4444444444");	/* process 4 */
-			Exit ();
-		}
-
-		SlowPrintf (7, "2222222222");		/* process 2 */
-		Exit ();
-	}
-
-	if (Fork () == 0) {
-
-		SlowPrintf (7, "3333333333");		/* process 3 */
-		Exit ();
-	}
-
-	SlowPrintf (7, "1111111111");			/* process 1 */
-	Exit ();
+      int yieldID;
+          if (Fork () == 0) {
+                    if (Fork () == 0) {
+                                  /* Process 4 */
+                                  SlowPrintf (7, "444444444444444444");
+                                              Exit ();
+                                                      }
+                            /* Process 2 */
+                            SlowPrintf (7, "222222222222222222");
+                                    Exit ();
+                                        }
+           
+              if ((yieldID = Fork ()) == 0) {
+                        /* Process 3 */   
+                        Yield (2);
+                                SlowPrintf (7, "333333333333333333");
+                                        Exit ();
+                                            }
+               
+                  /* Process 1 */
+                  Yield (yieldID);
+                      SlowPrintf (7, "111111111111111111");
+                          Exit ();
 }
